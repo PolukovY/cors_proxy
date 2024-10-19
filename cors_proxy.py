9a5e25aa97454367758c02214f1ed6bb33db7735
Yevgen.P <yevgen.polukov@gmail.com>
2024-10-19 13:08:49 +0300

    delete existing Access-Control-Allow-Origin.

--- cors_proxy.py
+++ cors_proxy.py
@@ -43,6 +43,9 @@ def proxy():
             return jsonify({'error': 'Unsupported HTTP method'}), 405
 
         response = Response(resp.content, resp.status_code, resp.headers.items())
+        if 'Access-Control-Allow-Origin' in response.headers:
+            del response.headers['Access-Control-Allow-Origin']
+
         response.headers['Access-Control-Allow-Origin'] = '*'
         response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
         response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization, x-api-key, Accept, Content-Type'
